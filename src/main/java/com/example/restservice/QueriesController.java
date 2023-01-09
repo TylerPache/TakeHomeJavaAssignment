2da6f6f81a5bbfb7db416afd64237a5a30df0a5f
Tyler Pache <ttpache@gmail.com>
2023-01-09 07:53:06 -0500

    Error code 503 added

--- src/main/java/com/example/restservice/QueriesController.java
+++ src/main/java/com/example/restservice/QueriesController.java
@@ -3,10 +3,12 @@ package com.example.restservice;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.naming.ServiceUnavailableException;
 
@@ -25,7 +27,7 @@ public class QueriesController {
             map.clear();
             map.put("excess", query1.getExcess());
             System.out.println("excess: " + query1.getExcess());
-            throw new Exception();
+            throw new ResponseStatusException(HttpStatusCode.valueOf(503),"\"excess\": " + query1.getExcess());
         }
         return map;
     }
